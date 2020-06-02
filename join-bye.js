133ed1c457de12f3153e1202206eb61077882752
BigBossNoahL <63643071+BigBossNoahL@users.noreply.github.com>
2020-06-02 15:40:47 +0200

    join-bye.js

---
+++ join-bye.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const Dash = new Discord.Client();
+
+Dash.on("ready", () => {
+    bot.user.setActivity(`$help | !command`); //Game activity
+  });
+
+Dash.on("ready", function () {
+    console.log("join-bye.js connected!");
+  });
+
+Dash.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'join');
+  if (!channel) return;
+  channel.send(`${member}`).then(sent => {
+    sent.edit(JoinEmbed)
+  });
+});
+
+Dash.on('guildMemberRemove', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'join');
+    if (!channel) return;
+    channel.send(`${member} just leave the server.
+Bye`); //Bye message
+});
+
+const JoinEmbed = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setThumbnail('https://static.hitek.fr/img/actualite/2015/05/fb_terminator-skynet-00258822.jpg')
+    .setTitle(`Hello, welcome to this server!`)
+    .setDescription('Please read the rules and enjoy :yum:')
+
+Dash.login('NzA1NDg1ODA5NjY2NzUyNjEz.Xr_oeg.Ye9A0uoT-7ujEEOTPOK_WWiiwHw');
