6777f6a12771a9abb8f7ea2bd9f4be2f463820ce
BigBossNoahL <63643071+BigBossNoahL@users.noreply.github.com>
2020-06-02 15:33:39 +0200

    Embed.js

---
+++ Embed.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const Dash = new Discord.Client();
+
+var prefix = "$"
+
+Dash.on("ready", function () {
+  console.log("main.js connected!");
+});
+
+Dash.on("ready", () => {
+    bot.user.setActivity(`$help | !command`); //Game activity
+  });
+
+const HelpEmbed = new Discord.MessageEmbed()
+    .setTitle('=> Help <=')
+    .setDescription('Help page')
+    .setColor('RANDOM')
+    .addFields(
+        { name: "$info", value: `Information about this bot`},
+        { name: "$tools", value: `Bot's tools`},
+        { name: "$developers", value: `Developers page`},
+        { name: "$ping", value: `Bot's answering time in 'ms'`},
+    )
+Dash.on('message', message => {
+    if (message.content.startsWith(prefixA + "help")) {
+        message.channel.send(HelpEmbed)
+    }
+});
